0446dad2b3e44f08339c3d798acf0b5b02d83653
Afzalun nesa Adita <afzalunnesaadita@gmail.com>
2026-05-15 22:27:37 +0600

    /services/notification.py (core)

---
+++ core/services/notification.py
@@ -0,0 +1,15 @@
+from delivery.services.notification import (
+    BaseNotification,
+    EmailNotification,
+    SMSNotification,
+    SystemNotification,
+    notify,
+)
+
+__all__ = [
+    "BaseNotification",
+    "EmailNotification",
+    "SMSNotification",
+    "SystemNotification",
+    "notify",
+]
