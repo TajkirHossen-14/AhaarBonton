87a652d2afe4095796dfe36281637d56df9d9dc9
Jannatul Ferdaus Khusbu Upoma <khushbukhushbu977@gmail.com>
2026-05-16 16:25:38 +0600

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,9 +1,1 @@
-from food.forms import FoodPostForm
-from users.forms import CustomLoginForm, CustomRegistrationForm, ProfileUpdateForm
 
-__all__ = [
-    "CustomRegistrationForm",
-    "CustomLoginForm",
-    "FoodPostForm",
-    "ProfileUpdateForm",
-]
